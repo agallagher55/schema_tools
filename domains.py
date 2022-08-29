d66610bb87ba31a17f5549d9a6bfbbd93ef785c0
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2022-08-29 16:22:00 -0300

    Update domains.py

--- domains.py
+++ domains.py
@@ -15,22 +15,15 @@ def add_code_value(workspace, domain_name, code, value):
 
 
 def remove_code_value(workspace, domain_name, code):
+    # TODO: Check if code exists first
 
     print(f"\nRemoving domain code: '{code}' from {domain_name}...")
-    coded_values = [d.codedValues for d in arcpy.da.ListDomains(workspace) if d.name == domain_name]
-
-    if coded_values:
-        codes = list(coded_values[0].keys())
-        if code not in codes:
-            print("\tCode not found in domain!")
-            return False
 
     arcpy.DeleteCodedValueFromDomain_management(
         workspace,
         domain_name,
         code
     )
-    return True
 
 
 def domain_in_db(db, domain):
@@ -49,12 +42,12 @@ def domain_in_db(db, domain):
     return True, domains
 
 
-def assign_to_field(feature, domain_name, field_name):
+def assign_to_field(feature, domain_name, field_name, subtypes: list):
     print(f"\nAssigning domain '{domain_name}' to field '{field_name}...'")
 
     arcpy.AssignDomainToField_management(
         in_table=feature,
         field_name=field_name,
         domain_name=domain_name,
-        subtype_code=None  # Optional
+        subtype_code=subtypes  # Optional
     )
