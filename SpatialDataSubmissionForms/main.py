1127ac5ae8e6e98fe78703ff32426e002e7868f2
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2022-09-05 20:04:13 -0300

    Update main.py

--- SpatialDataSubmissionForms/main.py
+++ SpatialDataSubmissionForms/main.py
@@ -42,35 +42,39 @@ if unfound_sf_domains:
     raise ValueError(f"A domain is referenced that is NOT found in SDE or as a new domain in the SDSF.\n\t{', '.join(unfound_sf_domains)}")
 
 # Transfer domains from SDE to workspace
-# for domain in sde_transfer_domains:
-#
-#     table = os.path.join(gdb, f"{domain}_domain")
-#     print(f"\nDomain ('{domain}') to table: '{table}'...")
-#
-#     domain_tbl = arcpy.DomainToTable_management(
-#         in_workspace=SDE_PROD_RW,
-#         domain_name=domain,
-#         out_table=table,
-#         code_field="CODE",
-#         description_field="VALUE"
-#     ).getOutput(0)
-#
-#     # Add domains from table to workspace
-#     print("\tCreating domain from table...")
-#     arcpy.TableToDomain_management(
-#         in_table=domain_tbl,
-#         code_field="CODE",
-#         description_field="VALUE",
-#         in_workspace=gdb,
-#         domain_name=domain,
-#         domain_description=domain
-#     )
+for domain in sde_transfer_domains:
+
+    table = os.path.join(gdb, f"{domain}_domain")
+    print(f"\nDomain ('{domain}') to table: '{table}'...")
+
+    domain_tbl = arcpy.DomainToTable_management(
+        in_workspace=SDE_PROD_RW,
+        domain_name=domain,
+        out_table=table,
+        code_field="CODE",
+        description_field="VALUE"
+    ).getOutput(0)
+
+    # Add domains from table to workspace
+    print("\tCreating domain from table...")
+    try:
+        arcpy.TableToDomain_management(
+            in_table=domain_tbl,
+            code_field="CODE",
+            description_field="VALUE",
+            in_workspace=gdb,
+            domain_name=domain,
+            domain_description=domain
+        )
+    except arcpy.ExecuteError as e:
+        print(e)
+        print(arcpy.GetMessages(2))
 
 # Create new Domains
 domain_dfs = submission_form.new_domains()
 print("\nSetting up new domains...")
-for domain in domain_dfs:
-    print(domain)
+for count, domain in enumerate(domain_dfs, start=1):
+    print(f"{count}/{len(domain_dfs)}) {domain}")
     domain_info = domain_dfs[domain]
 
     # TODO: Seem to have to manually create domains
@@ -99,36 +103,36 @@ for domain in domain_dfs:
             print(arcpy.GetMessages(2))
 
 
-# feature_schema_info = submission_form.df
-# for index, row in feature_schema_info.iterrows():
-#     field_name = row[0]
-#     field_index = row[1]
-#     field_type = row[2]
-#     field_length = row[3] if not pd.isna(row[3]) else None
-#     field_alias = row[4]
-#     field_desc = row[5]
-#     field_domain = row[6] if not pd.isna(row[6]) else None  # TODO: No domains being applied
-#     field_default = row[7] if not pd.isna(row[7]) else None
-#     field_isnullable = row[8]
-#     field_precision = row[9] if not pd.isna(row[9]) else None
-#     field_scale = row[10] if not pd.isna(row[10]) else None
-#
-#     print(f"{index}/{len(feature_schema_info.iterrows())}) {field_name}")
-#
-#     # Apply field mapping
-#     print(f"\tAdding field '{field_name}'...")
-#     arcpy.AddField_management(
-#         in_table=new_feature,
-#         field_name=field_name,
-#         field_type=field_type,  # TEXT, FLOAT, DOUBLE, SHORT, LONG, DATE, BLOB, RASTER, GUID
-#         field_precision=field_precision,
-#         field_scale=field_scale,  # For FLOAT, DOUBLE field types
-#         field_length=field_length,  # For TEXT field types
-#         field_alias=field_alias,
-#         field_is_nullable=field_isnullable,  # NULLABLE, NON_NULLABLE
-#         # field_is_required="#",  # NON_REQUIRED, REQUIRED
-#         field_domain=field_domain
-#     )
+feature_schema_info = submission_form.df
+for index, row in feature_schema_info.iterrows():
+    field_name = row[0]
+    field_index = row[1]
+    field_type = row[2]
+    field_length = row[3] if not pd.isna(row[3]) else None
+    field_alias = row[4]
+    field_desc = row[5]
+    field_domain = row[6] if not pd.isna(row[6]) else None  # TODO: No domains being applied
+    field_default = row[7] if not pd.isna(row[7]) else None
+    field_isnullable = row[8]
+    field_precision = row[9] if not pd.isna(row[9]) else None
+    field_scale = row[10] if not pd.isna(row[10]) else None
+
+    print(f"{index}/{len(feature_schema_info.iterrows())}) {field_name}")
+
+    # Apply field mapping
+    print(f"\tAdding field '{field_name}'...")
+    arcpy.AddField_management(
+        in_table=new_feature,
+        field_name=field_name,
+        field_type=field_type,  # TEXT, FLOAT, DOUBLE, SHORT, LONG, DATE, BLOB, RASTER, GUID
+        field_precision=field_precision,
+        field_scale=field_scale,  # For FLOAT, DOUBLE field types
+        field_length=field_length,  # For TEXT field types
+        field_alias=field_alias,
+        field_is_nullable=field_isnullable,  # NULLABLE, NON_NULLABLE
+        # field_is_required="#",  # NON_REQUIRED, REQUIRED
+        field_domain=field_domain
+    )
 
 
     # Set field default
