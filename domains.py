457a99b53f26ac972197d83e3cc1a2ca16ef93f2
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2022-10-10 10:20:21 -0300

    Update domains.py

--- domains.py
+++ domains.py
@@ -1,8 +1,31 @@
 import arcpy
+import functools
 
 from typing import Sequence
 
 
+def arcpy_messages(func):
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        try:
+            result = func(*args, **kwargs)
+            messages = arcpy.GetMessages()
+            message_lines = messages.split("\n")
+
+            for message in message_lines:
+                if message:
+                    print(f"\t{message}")
+
+            return result
+
+        except arcpy.ExecuteError as e:
+            print(f"ARCPY ERROR: {e}")
+
+    return wrapper
+
+
+@arcpy_messages
 def add_code_value(workspace, domain_name, code, value):
     # TODO: Check if code already exists
 
@@ -16,6 +39,7 @@ def add_code_value(workspace, domain_name, code, value):
     )
 
 
+@arcpy_messages
 def remove_code_value(workspace, domain_name, code):
     # TODO: Check if code exists first
 
@@ -28,60 +52,63 @@ def remove_code_value(workspace, domain_name, code):
     )
 
 
-def domain_in_db(db, domain):
+@arcpy_messages
+def domains_in_db(db, domains):
     """
     - Make sure domain exists in database
     :param db:
-    :param domain:
-    :return:
-    """
-
-    domains = [domain.name for domain in arcpy.da.ListDomains(db)]
-
-    if domain not in domains:
-        return False, domains
-
-    return True, domains
-
-
-def assign_to_field(feature, domain_name, field_name, subtypes: list):
-    print(f"\nAssigning domain '{domain_name}' to field '{field_name}...'")
-
-    arcpy.AssignDomainToField_management(
-        in_table=feature,
-        field_name=field_name,
-        domain_name=domain_name,
-        subtype_code=subtypes  # Optional
-    )
-
-
-def transfer_domains(domains: Sequence[str], output_workspace, from_workspace):
-    """
-
     :param domains:
-    :param output_workspace:
-    :param from_workspace:
     :return:
+    """
+
+    print(f"\nChecking to see if domains {', '.join(domains)} exist in {db}...")
+
+    db_domains = [domain.name for domain in arcpy.da.ListDomains(db)]
+    unfound_domains = [d for d in domains if d not in db_domains]
+
+    if unfound_domains:
+        return False, unfound_domains
+
+    return True, unfound_domains
+
+
+@arcpy_messages
+def assign_to_field(feature, domain_name, field_name, subtypes):
+    print(f"\nAssigning domain '{domain_name}' to field '{field_name}...'")
+    try:
+        arcpy.AssignDomainToField_management(
+            in_table=feature,
+            field_name=field_name,
+            domain_name=domain_name,
+            subtype_code=subtypes  # Optional
+        )
+    except arcpy.ExecuteError as e:
+        print(e)
+
+
+@arcpy_messages
+def transfer_domains(domains: Sequence[str], output_workspace, from_workspace) -> dict:
+    """
+    - Transfer domains from from_workspace to output_workspace. Only domain types of CODED, TEXT will be valid
+
+    :param domains: domains to transfer
+    :param output_workspace: where domains will be trasferred to
+    :param from_workspace: will domains will be transferred from
+    :return: {"unfound_domains": unfound_domains, "domains": domains}
     """
 
     print(f"\nTransferring domains from {from_workspace} to {output_workspace}...")
-
     from_workspace_domains = {x.name: x.codedValues for x in arcpy.da.ListDomains(from_workspace)}
     output_workspace_domains = [x.name for x in arcpy.da.ListDomains(output_workspace)]
-    
+
     unfound_domains = list()
 
     for count, domain in enumerate(domains, start=1):
         print(f"\n{count}/{len(domains)}) Domain: '{domain}'")
 
-        domain_info = from_workspace_domains[domain]
-
-        # Check that domain is in output_workspace_domains
-        if domain not in from_workspace_domains:
-            unfound_domains.append(domain)
-            print(f"Did not find '{domain}' in source workspace - {output_workspace}")
-
+        # Check that domain doesn't already exist in output workspace
         if domain not in output_workspace_domains:
+
             arcpy.CreateDomain_management(
                 in_workspace=output_workspace,
                 domain_name=domain,
@@ -90,14 +117,24 @@ def transfer_domains(domains: Sequence[str], output_workspace, from_workspace):
                 domain_type="CODED"
             )
 
-            for code, value in domain_info.items():
-                print(f"\tAdding ({code}: {value})")
-                arcpy.AddCodedValueToDomain_management(
-                    in_workspace=output_workspace,
-                    domain_name=domain,
-                    code=code,
-                    code_description=value
-                )
+            # Check that domain is in source workspace
+            if domain not in from_workspace_domains:
+                unfound_domains.append(domain)
+                print(f"\tDid NOT find '{domain}' in source workspace - {from_workspace}")
+
+                # Will need to get code, values from spreadsheet
+
+            else:
+                domain_info = from_workspace_domains.get(domain)
+
+                for code, value in domain_info.items():
+                    print(f"\tAdding ({code}: {value})")
+                    arcpy.AddCodedValueToDomain_management(
+                        in_workspace=output_workspace,
+                        domain_name=domain,
+                        code=code,
+                        code_description=value
+                    )
 
         else:
             print(f"\t'{domain}' is already in the output workspace!")
