82d1e6926181729ff670542be15034e6533c6a03
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2022-10-10 10:19:43 -0300

    Update connections.py

--- connections.py
+++ connections.py
@@ -1,135 +1,329 @@
 import arcpy
-import time
+import os
+import functools
 
-database_conn = r'Database Connections\Enterprise.sde'
+from .reporter import FieldsReport
 
-# DEV
-dev_rw = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\DEV_RW_SDEADM.sde"
-dev_ro = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\DEV_RO_SDEADM.sde"
-dev_web_ro = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\webgis_DEV_RO.sde"  # Only need to include when adding a new domain to a field
-dev_web_ro_gdb = r"\\msfs06\GISApp\AGS_Dev\fgdbs\web_RO.gdb"
-dev_connections = [
-    dev_ro,
-    dev_web_ro,
-    dev_rw,
-    dev_web_ro_gdb
-]
+from .settings import (
+    EDITOR_TRACKING_FIELD_INFO,
+    SPATIAL_REFERENCE_FEATURE
+)
 
-qa_rw = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\QA_RW_SDEADM.sde"
-qa_ro = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\QA_RO_SDEADM.sde"
-qa_web_ro = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\QA_RO_webgis.sde"  # Only need to include when adding a new domain to a field
-qa_web_ro_gdb = r"\\msfs06\GISApp\AGS_QA\fgdbs\web_RO.gdb"
-
-qa_connections = [
-    qa_ro,
-    # qa_web_ro,
-    qa_web_ro_gdb,
-    qa_rw,
-
-]
+arcpy.env.overwriteOutput = True
 
 
-prod_rw = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\PROD_RW_SDEADM.sde"
-prod_ro = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\PROD_RO_SDEADM.sde"
-prod_ro_web = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\webgis_PROD_RO_win.sde"  # Only need to include when adding a new domain to a field
-prod_web_ro_gdb = r"\\msfs06\GISApp\AGS_Prod\fgdbs\web_RO.gdb"
+def arcpy_messages(func):
 
-prod_connections = [
-    prod_ro,
-    # prod_ro_web,
-    prod_web_ro_gdb,
-    prod_rw
-]
-
-
-def prevent_connections(sde):
-
-    with arcpy.EnvManager(workspace=sde):
-        # Block new connections to the database.
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
         try:
-            print(f'Blocking connections in {sde}...')
-            arcpy.AcceptConnections(sde, accept_connections=False)
+            result = func(*args, **kwargs)
+            messages = arcpy.GetMessages()
+            message_lines = messages.split("\n")
 
-        except(arcpy.ExecuteError, arcpy.ExecuteWarning) as e:
-            print(e)
-            print('Blocked all connections')
+            for message in message_lines:
+                if message:
+                    print(f"\t{message}")
 
-        time.sleep(60)  # wait 60 seconds
+            return result
 
-        # Disconnect all users from the database.
-        try:
-            print(f'Disconnecting all users from {sde}...')
-            arcpy.DisconnectUser(sde, users="ALL")
+        except arcpy.ExecuteError as e:
+            print(f"ARCPY ERROR: {e}")
 
-        except(arcpy.ExecuteError, arcpy.ExecuteWarning) as e:
-            print(e)
-            print('Disconnected all users')
+    return wrapper
 
 
-def allow_connections(sde):
+class Feature:
+    def __init__(self, workspace, feature_name: str, geometry_type, spatial_reference, alias: str = "#"):
+        self.workspace = workspace
+        self.feature_name = feature_name
+        self.geometry_type = geometry_type
+        self.spatial_reference = spatial_reference
+        self.alias = alias
+        
+        self.feature = os.path.join(self.workspace, self.feature_name)
+        self.fields = set()
 
-    # Allow the database to begin accepting connections again
-    try:
-        print(f'Accepting Connections from {sde}...')
-        arcpy.AcceptConnections(sde, accept_connections=True)
+        self.create_feature()
+        self.desc = arcpy.Describe(self.feature)
 
-    except(arcpy.ExecuteError, arcpy.ExecuteWarning) as e:
-        print(e)
+    @arcpy_messages
+    def create_feature(self):
+        print(f"\nCreating {self.geometry_type or ''} Feature class '{self.feature_name}'...")
 
+        # Check if feature already exists
+        if arcpy.Exists(self.feature):
+            print("\tFeature already exists!")
+            self.fields = {x.name for x in arcpy.ListFields(self.feature)}
+            return self.feature
 
-def check_connections(db_connections: [[]]):
-    # Make sure all db connection files are valid
-    print(f"\nChecking connections...")
+        arcpy.CreateFeatureclass_management(
+            out_path=self.workspace,
+            out_name=self.feature_name,
+            geometry_type=self.geometry_type,
+            spatial_reference=self.spatial_reference,
+            out_alias=self.alias
+        )
 
-    fails = []
-    success = []
+        self.fields = {x.name for x in arcpy.ListFields(self.feature)}
+        print("\tFeature Created.")
 
-    for connections in db_connections:
-        for connection in connections:
+        return self.feature
 
-            if not arcpy.Exists(connection):
-                fails.append(connection)
+    @arcpy_messages
+    def add_field(self, field_name: str, field_type: str, length: int, alias: str, nullable: str, domain_name: str):
+        """
+           :param field_name:
+           :param field_type:
+           :param length:
+           :param alias:
+           :param nullable:
+           :param domain_name:
+           :return:
+           """
 
-            else:
-                success.append(connection)
+        # If field already exists, skip
+        if field_name in self.fields:
+            print(f"\t'{field_name}' already exists in {self.feature_name}")
+            return True
 
-    if fails:
-        raise IndexError(f"Invalid connection(s) found: {', '.join(fails)}")
+        FIELD_REQUIRED = "NON_REQUIRED"
+        valid_types = ["TEXT", "FLOAT", "DOUBLE", "SHORT", "LONG", "DATE"]
 
-    return {"valid_connections": success}
+        if field_type:
+            field_type = field_type.upper()
+
+        if field_type == "STRING":
+            field_type = "TEXT"
+
+        nullable_mapping = {
+            1.0: "NULLABLE",
+            0.0: "NON_NULLABLE",
+        }
+
+        if nullable in nullable_mapping:
+            nullable = nullable_mapping[nullable]
+
+        if field_type not in valid_types:
+            print(f"Field Type: {field_type}")
+            raise ValueError(f"Field type: '{field_type}' does not appear to be a valid field type!")
+
+        if nullable not in ("NULLABLE", "NON_NULLABLE", None, "#"):
+            raise ValueError(f"Nullable Value, '{nullable}' does not appear to be a valid value!")
+
+        if field_type == "SHORT":
+            field_precision = input("Please provide field precision: ")
+        else:
+            field_precision = "#"  # int
+
+        print(f"\nAdding Field: '{field_name}'...")
+
+        arcpy.AddField_management(
+            in_table=self.feature,
+            field_name=field_name,  # Field Name
+            field_type=field_type,  # Field Type
+            field_precision=field_precision,
+            field_scale="#",
+            field_length=length,  # Field Length (# of characters)
+            field_alias=alias,  # Alias
+            field_is_nullable=nullable,  # NULLABLE
+            field_is_required=FIELD_REQUIRED,  #
+            field_domain=domain_name  # Domain
+        )
+        self.fields.add(field_name)
+
+    @arcpy_messages
+    def change_privileges(self, user: str, view: str = "#", edit: str = "#"):
+        """
+        :param user: The database username whose privileges are being modified.
+        :param view: Establishes the user's view privileges.
+                        AS_IS — No change to the user's existing view privilege.
+                        If the user has view privileges, they will continue to have view privileges.
+                        If the user doesn't have view privileges, they will continue to not have view privileges
+
+                        GRANT —Allows user to view datasets.
+
+                        REVOKE —Removes all user privileges to view datasets.
+
+        :param edit: Establishes the user's edit privileges.
+                        AS_IS — No change to the user's existing edit privilege.
+                        If the user has edit privileges, they will continue to have edit privileges.
+                        If the user doesn't have edit privileges, they will continue to not have edit privileges
+                        This is the default.
+
+                        GRANT —Allows the user to edit the input datasets.
+                        REVOKE —Removes the user's edit privileges. The user may still view the input dataset.
+        :return:
+        """
+
+        print(f"\nChanging privileges to dataset '{self.feature}'...")
+        arcpy.ChangePrivileges_management(
+            in_dataset=self.feature,
+            user=user,
+            View=view,
+            Edit=edit
+        )
+        # arcpy.ChangePrivileges_management(GDB_Name + "\\" + RC_FC, "PUBLIC", "GRANT", "#")
+        # arcpy.ChangePrivileges_management(GDB_Name + "\\" + RC_FC, "TRAFFIC_EDITOR", "GRANT", "GRANT")
+        print("\tPrivileges changed.")
+
+    @arcpy_messages
+    def register_as_versioned(self, edit_to_base: str = "#"):
+        """
+        :param edit_to_base: Specifies whether edits made to the default version will be moved to the
+                                base tables. This parameter is not applicable for branch versioning.
+
+                            NO_EDITS_TO_BASE — The dataset will not be versioned with the option of moving
+                                                edits to base. This is the default.
+                            EDITS_TO_BASE — The dataset will be versioned with the option of moving edits to base
+        :return:
+        """
+
+        print(f"\nRegistering dataset '{self.feature}' as versioned.")
+        arcpy.RegisterAsVersioned_management(
+            self.feature,
+            edit_to_base
+        )
+        print("\tDataset registered.")
+
+    @arcpy_messages
+    def add_editor_tracking_fields(self, field_info=EDITOR_TRACKING_FIELD_INFO):
+        print(f"\nAdding Editor Tracking fields to '{self.feature}'...")
+
+        for field in field_info:
+            if field not in self.fields:
+                print(f"\tAdding '{field}' field...")
+
+                field_type = field_info[field]["field_type"]
+                field_length = field_info[field]["field_length"]
+                field_alias = field_info[field]["field_alias"]
+
+                arcpy.AddField_management(self.feature, field, field_type, "", "", field_length, field_alias)
+                self.fields.add(field)
+
+    @arcpy_messages
+    def enable_editor_tracking(self, creator_field: str = "ADDBY", creation_date_field: str = "ADDDATE",
+                               last_editor_field: str = "MODBY", last_edit_date_field: str = "MODDATE",
+                               add_fields: str = "NO_ADD_FIELDS", record_dates_in: str = "UTC"):
+
+        print(f"\nApplying editor tracking to {self.feature}...")
+
+        if self.desc.editorTrackingEnabled:
+            print(f"\t{self.feature} already had Editor Tracking Enabled!")
+
+        arcpy.EnableEditorTracking_management(
+            self.feature,
+            creator_field,
+            creation_date_field,
+            last_editor_field,
+            last_edit_date_field,
+            add_fields,
+            record_dates_in
+        )
+
+    @arcpy_messages
+    def add_gloablids(self):
+        print(f"\nAdding GloablIDs to '{self.feature}'...")
+
+        if "GlobalID" in self.fields:
+            print(f"{self.feature} already has a GlobalID field.")
+            return True
+
+        arcpy.AddGlobalIDs_management(self.feature)
+
+    @arcpy_messages
+    def add_field_default(self, field: str, value):
+        print(f"\nAssigning default value of '{value}' to {field}...")
+
+        # TODO: Check if field already has default applied.
+
+        arcpy.AssignDefaultToField_management(
+            in_table=self.feature,
+            field_name=field,
+            default_value=value
+        )
+
+    @arcpy_messages
+    def assign_domain(self, field_name, domain_name, subtypes="#"):
+        print(f"\nAssigning domain '{domain_name}' to field '{field_name}...'")
+
+        # TODO: Check if field already has domain assigned.
+
+        arcpy.AssignDomainToField_management(
+            in_table=self.feature,
+            field_name=field_name,
+            domain_name=domain_name,
+            subtype_code=subtypes  # Optional
+        )
 
 
 if __name__ == "__main__":
-    # DEV
-    dev_rw = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\DEV_RW_SDEADM.sde"
-    dev_ro = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\DEV_RO_SDEADM.sde"
-    dev_web_ro = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\webgis_DEV_RO.sde"  # Only need to include when adding a new domain to a field?
-    dev_gdbs = [dev_ro, dev_rw, dev_web_ro]
+    for excel_file in EXCEL_FILES:
+        print(f"\nEXCEL FILE: {excel_file}")
 
+        fields_report = FieldsReport(excel_file)
 
+        feature_name = fields_report.feature_class_name
+        feature_shape = fields_report.feature_shape
+        field_data = fields_report.field_details
 
-    # QA
-    qa_rw = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\QA_RW_SDEADM.sde"
-    qa_ro = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\QA_RO_SDEADM.sde"
-    qa_web_ro = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\QA_RO_webgis.sde"  # Only need to include when adding a new domain to a field?
-    qa_gdbs = [qa_rw, qa_ro, qa_web_ro]
+        if not feature_shape:
+            print("No shape type specified")
 
-    # PROD
-    prod_rw = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\PROD_RW_SDEADM.sde"
-    prod_ro = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\PROD_RO_SDEADM.sde"
-    prod_ro_web = "C:\\Users\\gallaga\\AppData\\Roaming\\Esri\\ArcGISPro\\Favorites\\webgis_PROD_RO_win.sde"  # Only need to include when adding a new domain to a field?
-    prod_gdbs = [prod_rw, prod_ro, prod_ro_web]
+        if feature_shape == "Line":
+            feature_shape = "Polyline"
 
-    dev_web_ro_gdb = r"\\msfs06\GISApp\AGS_Dev\fgdbs\web_RO.gdb"
-    qa_web_ro_gdb = r"\\msfs06\GISApp\AGS_QA\fgdbs\web_RO.gdb"
-    prod_web_ro_gdb = r"\\msfs06\GISApp\AGS_Prod\fgdbs\web_RO.gdb"
+        print(f"\nFeature Name: {feature_name}")
+        print(f"Feature Shape: {feature_shape}")
 
-    web_fs_gdbs = [prod_web_ro_gdb, dev_web_ro_gdb, qa_web_ro_gdb]
+        feature = Feature(
+            workspace=WORKING_GDB,
+            feature_name=feature_name,
+            geometry_type=feature_shape,
+            spatial_reference=SPATIAL_REFERENCE_FEATURE
+        )
 
-    # connections = check_connections([
-    #     dev_gdbs,
-    #     qa_gdbs,
-    #     prod_gdbs,
-    #     web_fs_gdbs,
-    # ]
-    # )
+        print("\nAdding Fields...")
+
+        ignore_fields = {"OBJECTID", "GLOBALID", "ADDBY", "ADDDATE", "MODBY", "MODDATE", "OBJECTID", "SHAPE",
+                         "SHAPE_AREA", "SHAPE_LENGTH"}
+        feature_fields = field_data["Field Name"]
+
+        for field, row in field_data.iterrows():  # TODO: shape area, shape length fields is appearing first
+            field_name = row["Field Name"].upper()
+
+            if field_name in ignore_fields:
+                continue
+
+            field_length = row["Field Length (# of characters)"]
+            if field_length:
+                field_length = int(field_length)
+
+            if not field_length and not field_type != "TEXT":
+                raise ValueError(f"Field of type {field_type} needs to have a field length.")
+
+            alias = row["Alias"]
+            field_type = row["Field Type"]
+            field_len = field_length
+            nullable = row["Nullable"] or "NON_NULLABLE"
+            default_value = row["Default Value"]
+            domain = row["Domain"] or "#"
+            notes = row["Notes"]
+
+            feature.add_field(
+                field_name=field_name,
+                field_type=field_type,
+                length=field_len,
+                alias=alias,
+                nullable=nullable,
+                domain_name=domain
+            )
+
+        # ADD GLOBAL IDS
+        feature.add_gloablids()
+
+        # ADD EDITOR TRACKING FIELDS
+        feature.add_editor_tracking_fields(EDITOR_TRACKING_FIELD_INFO)
+
+        # ENABLE EDITOR TRACKING
+        feature.enable_editor_tracking()
