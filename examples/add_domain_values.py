b8fa431b2cf69c008e6afbf2de0192c96c4a7109
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2022-12-08 16:55:53 -0400

    Update add_domain_values.py

--- examples/add_domain_values.py
+++ examples/add_domain_values.py
@@ -10,15 +10,18 @@ arcpy.SetLogHistory(False)
 
 CURRENT_DIR = getcwd()
 
-DOMAIN_NAME = "AAA_operator_asset"
+DOMAIN_NAME = "LND_gflum_DART"
 ADD_CODE_VALUES = {
-    "ME24191": "Melanie Parker",
+    "CDD": "CDD",
 }
+
+# Value needs to be CDD. All other values in GFLUM domain are the same as the code.
+# --> The field has a max length of 12 - wouldn't fit full name anyway.
 
 if __name__ == "__main__":
     SCRATCH_GDB = utils.create_fgdb(CURRENT_DIR)
 
-    # operator_domain = domains.transfer_domains([DOMAIN_NAME, ], SCRATCH_GDB, connections.prod_rw)
+    # operator_domain = domains.transfer_domains([DOMAIN_NAME, ], SCRATCH_GDB, from_workspace=connections.prod_rw)
 
     for dbs in [
         # [SCRATCH_GDB]
@@ -30,7 +33,7 @@ if __name__ == "__main__":
         print(f"\nProcessing dbs: {', '.join(dbs)}...")
 
         for db in dbs:
-            print(f"\n\tDatabase: {db}")
+            print(f"\nDATABASE: {db}")
 
             # Check that domain is found in database connection
             domain_found, db_domains = domains.domains_in_db(db, [DOMAIN_NAME])
