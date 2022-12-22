e897c6c7d6b190d606b7e1bfbb32a35e0979b2d8
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2022-12-22 14:08:45 -0400

    Update add_domain_values.py

--- examples/add_domain_values.py
+++ examples/add_domain_values.py
@@ -10,22 +10,39 @@ arcpy.SetLogHistory(False)
 
 CURRENT_DIR = getcwd()
 
-DOMAIN_NAME = "LND_gflum_DART"
-ADD_CODE_VALUES = {
-    "CDD": "CDD",
-}
+"""
+    LND_outdoor_rec_use
+    Field: REC_USE
+    Domain: LND_rec_activeusearea_type
+    Code/Value: BPK/Bike Park Dirt Jump
+    
+    LND_park_rec_feature
+    Field: MAINRECUSE
+    Domain: LND_rec_bike_use
+    Code/Value: DIRT JUMP/Dirt Jump
+"""
 
-# Value needs to be CDD. All other values in GFLUM domain are the same as the code.
-# --> The field has a max length of 12 - wouldn't fit full name anyway.
+domain_change_info = {
+    "LND_rec_activeusearea_type": {
+        "BPK": "Pump Tracks"
+    },
+    "LND_rec_bike_use": {
+        "DIRT JUMP": "Pump Tracks"
+    }
+}
 
 if __name__ == "__main__":
     SCRATCH_GDB = utils.create_fgdb(CURRENT_DIR)
 
-    # operator_domain = domains.transfer_domains([DOMAIN_NAME, ], SCRATCH_GDB, from_workspace=connections.prod_rw)
+    domains.transfer_domains(
+        list(domain_change_info.keys()),
+        SCRATCH_GDB,
+        from_workspace=connections.prod_rw
+    )
 
     for dbs in [
-        # [SCRATCH_GDB]
-        connections.dev_connections,
+        [SCRATCH_GDB],
+        # connections.dev_connections,
         # connections.qa_connections,
         # connections.prod_connections
     ]:
@@ -35,14 +52,18 @@ if __name__ == "__main__":
         for db in dbs:
             print(f"\nDATABASE: {db}")
 
-            # Check that domain is found in database connection
-            domain_found, db_domains = domains.domains_in_db(db, [DOMAIN_NAME])
+            for domain in domain_change_info:
+                print(f"\tDOMAIN: {domain}")
 
-            if not domain_found:
-                raise ValueError(f"Did not find domain '{DOMAIN_NAME}' in db. Found domains: {', '.join(db_domains)}")
+                # Check that domain is found in database connection
+                domain_found, db_domains = domains.domains_in_db(db, [domain])
 
-            for code_value in ADD_CODE_VALUES:
-                new_code = code_value
-                new_value = ADD_CODE_VALUES[code_value]
+                if not domain_found:
+                    raise ValueError(f"Did not find domain '{domain}' in db. Found domains: {', '.join(db_domains)}")
 
-                domains.add_code_value(db, DOMAIN_NAME, new_code, new_value)
+                add_code_values = domain_change_info[domain]
+                for code_value in add_code_values:
+                    new_code = code_value
+                    new_value = add_code_values[code_value]
+
+                    domains.add_code_value(db, domain, new_code, new_value)
