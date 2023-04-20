fae27110814a91743a7e7a9597bad98fb81f8dd9
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2023-04-20 09:43:07 -0300

    Create features.py

---
+++ features.py
@@ -0,0 +1,35 @@
+import arcpy
+import os
+
+
+def transfer_features(features: list, output_workspace: str):
+
+    """
+    The transfer_features function takes a list of features and an output workspace as arguments.
+    It then copies each feature to the output workspace, returning a list of the copied features.
+
+    :param features: list: Pass in the list of features that are to be transferred
+    :param output_workspace: str: Specify the workspace where the features will be copied to
+    :return: A list of the copied features
+
+    """
+
+    out_features = list()
+
+    print(f"\nTransferring features to {output_workspace}...")
+
+    for feature in features:
+            desc = arcpy.Describe(feature)
+
+            feature_name = desc.name.replace("SDEADM.", "")
+            out_feature = os.path.join(output_workspace, feature_name)
+
+            arcpy.Copy_management(
+                in_data=feature,
+                out_data=out_feature
+            )
+            print(arcpy.GetMessages())
+
+            out_features.append(out_feature)
+
+    return out_features
