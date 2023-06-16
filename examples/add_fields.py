7f864bccfbfbe9ec02bf332e6f9ceffc39c691fe
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2023-06-16 10:58:02 -0300

    Update add_fields.py

--- examples/add_fields.py
+++ examples/add_fields.py
@@ -1,8 +1,6 @@
-import os.path
-
 import arcpy
 
-from features import Table
+from features import Feature
 import utils
 
 from configparser import ConfigParser
@@ -17,14 +15,16 @@ config.read('config.ini')
 
 CURRENT_DIR = getcwd()
 
-FEATURE = "SDEADM.AST_LIGHT_FIXTURE"  # TODO: this should be renamed so it doesn't clash with import
+FEATURE = "SDEADM.TRN_bus_stop"  # TODO: this should be renamed so it doesn't clash with import
+
+SPATIAL_REFERENCE = None
 
 new_field_info = {
-    "PWELID": {
-        "description": "Power Light Enclosure ID",
-        "alias": "Power Light Enclosure ID",
-        "field_type": "TEXT",
-        "field_length": "50",
+    "INSP_DATE": {
+        "description": "The date that the bus stop was last inspected by Halifax Transit Staff",
+        "alias": "Date Last Inspected",
+        "field_type": "DATE",
+        # "field_length": "50",
         "nullable": "NULLABLE",
         "default": "",
         "domain": ""
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     local_gdb = utils.create_fgdb(CURRENT_DIR)
 
     for dbs in [
-        # [local_gdb],
+        [local_gdb],
         # [
         #     config.get("SERVER", "dev_rw"),
         #     config.get("SERVER", "dev_ro"),
@@ -44,10 +44,10 @@ if __name__ == "__main__":
         #     config.get("SERVER", "qa_rw"),
         #     config.get("SERVER", "qa_ro")
         # ],
-        [
-            config.get("SERVER", "prod_rw"),
-            config.get("SERVER", "prod_ro")
-        ],
+        # [
+        #     config.get("SERVER", "prod_rw"),
+        #     config.get("SERVER", "prod_ro")
+        # ],
 
     ]:
 
@@ -65,7 +65,7 @@ if __name__ == "__main__":
 
                 desc = arcpy.Describe(FEATURE)
 
-                my_feature = Table(db, desc.baseName)
+                my_feature = Feature(db, desc.baseName, "POINT")
                 current_fields = [x.name for x in arcpy.ListFields(FEATURE)]
 
                 # TODO: Stop services
@@ -82,23 +82,10 @@ if __name__ == "__main__":
                     my_feature.add_field(
                         field_name=field,
                         field_type=new_field_info[field]["field_type"],
-                        length=new_field_info[field]["field_length"],
+                        length=new_field_info[field].get("field_length", "#"),
                         alias=new_field_info[field]["alias"],
                         domain_name=new_field_info[field]["domain"]
                     )
-
-                    # TODO: Index Field
-                    # try:
-                    print("Adding index...")
-                    arcpy.AddIndex_management(
-                        in_table=my_feature.feature_name,
-                        fields=field,
-                        index_name=f"index_{field}",
-                        ascending="ASCENDING"
-                    )
-                    # except arcpy.ExecuteError:
-                    #     arcpy_msg = arcpy.GetMessages(2)
-                    #     print(arcpy_msg)
 
                 # TODO: Start services
                 # * Had to manually unlock with SDE connection
