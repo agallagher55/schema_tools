801adb4b2a8d25a9528f6cb75dd4d5d3944de189
Alex Gallagher <37911372+agallagher55@users.noreply.github.com>
2023-12-27 10:11:33 -0400

    Update change_domain_values.py

--- examples/change_domain_values.py
+++ examples/change_domain_values.py
@@ -65,42 +65,42 @@ if __name__ == "__main__":
     ]:
         if dbs:
             print(f"\nProcessing dbs: {', '.join(dbs)}...")
-    
+
             for db in dbs:
                 print(f"\nDATABASE: {db}")
-    
+
                 if db == local_gdb:
-    
+
                     # Check for domains in local workspace
                     domain_present, unfound_domains = domains.domains_in_db(local_gdb, list(ADD_CODE_VALUES.keys()))
-    
+
                     if unfound_domains:
                         prod_sde = config.get("SERVER", "prod_rw") if "APP" in PC_NAME else config.get("LOCAL", "prod_rw")
-    
+
                         domains.transfer_domains(
                             list(ADD_CODE_VALUES.keys()),
                             local_gdb,
                             from_workspace=prod_sde
                         )
-    
+
                 for domain in REMOVE_CODE_VALUES:
-                    remove_codes = list(REMOVE_CODE_VALUES[domain].keys())
-    
-                    for count, code in enumerate(remove_codes, start=1):
-                        domains.remove_code_value(db, domain, code)
-    
+                    remove_codes = list(REMOVE_CODE_VALUES.values())
+
+                    for count, domain_code in enumerate(remove_codes, start=1):
+                        domains.remove_code_value(db, domain, domain_code)
+
                 for domain in ADD_CODE_VALUES:
                     print(f"\tDOMAIN: {domain}")
-    
+
                     # Check that domain is found in database connection
                     domain_found, db_domains = domains.domains_in_db(db, [domain])
-    
+
                     if not domain_found:
                         raise ValueError(f"Did not find domain '{domain}' in db. Found domains: {', '.join(db_domains)}")
-    
+
                     add_code_values = ADD_CODE_VALUES[domain]
                     for count, code_value in enumerate(add_code_values, start=1):
                         new_value = add_code_values[code_value]
-    
+
                         print(f"\n{count}/{len(add_code_values)}) Domain and Code: {code_value} & {new_value}")
                         domains.add_code_value(db, domain, code_value, new_value)
